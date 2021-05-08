fde7b0ec58709c380bb530bf3d894675dcbcee41
kcprnoj <54915392+kcprnoj@users.noreply.github.com>
2021-05-08 18:40:59 +0200

    Added List Adapter

--- app/src/main/java/com/main/MainActivity.kt
+++ app/src/main/java/com/main/MainActivity.kt
@@ -2,15 +2,20 @@ package com.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.main.R
 import com.main.database.RunDAO
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var runDAO: RunDAO
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = RunListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/com/main/RunListAdapter.kt
@@ -0,0 +1,53 @@
+package com.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.main.R
+import com.main.database.Run
+
+class RunListAdapter : ListAdapter<Run, RunListAdapter.RunViewHolder>(RunsComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RunViewHolder {
+        return RunViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: RunViewHolder, position: Int) {
+        var current = getItem(position)
+        holder.bind(current.id.toString() + " " + current.distance.toString() + "km")
+    }
+
+    class RunViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        private val runItemView: TextView = itemView.findViewById(R.id.textView)
+
+        fun bind(text: String?) {
+            runItemView.text = text
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): RunViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.training_item, parent, false)
+                return RunViewHolder(view)
+            }
+        }
+    }
+
+    class RunsComparator : DiffUtil.ItemCallback<Run>() {
+        override fun areItemsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem.avgSpeed == newItem.avgSpeed &&
+                    oldItem.distance == newItem.distance &&
+                    oldItem.timestamp == newItem.timestamp &&
+                    oldItem.calories == newItem.calories &&
+                    oldItem.time == newItem.time
+        }
+    }
+}
